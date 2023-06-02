b95e4d3dc39eddc41b537ebc71a92e357871dd65
Saurelien <aurelienschmitt.info@gmail.com>
2023-06-02 09:27:07 +0200

    Refectoring the structure of the project

--- litreview/ticket/models.py
+++ litreview/ticket/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=200)
+    descritpion = models.TextField(max_length=2048)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on delete=)
