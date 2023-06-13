ff0808bf2b3471e6c83c570d90f6c0264c650989
Saurelien <aurelienschmitt.info@gmail.com>
2023-06-13 21:31:30 +0200

    Specified il a field is not filled a message should pop up but not in french unfortunatly.

--- authenticator/forms.py
+++ authenticator/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+
 
 class RegistrationForm(UserCreationForm):
-    email = forms.EmailField(label="Adresse e-mail")
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
+    email = forms.EmailField(label="Adresse e-mail", error_messages={'required': "Veuillez remplir ce champ*"})
+    first_name = forms.CharField(label="Prénom", error_messages={'required': "Veuillez remplir ce champ*"})
+    last_name = forms.CharField(label="Nom de famille", error_messages={'required': "Veuillez remplir ce champ*"})
+    password1 = forms.CharField(label="Mot de passe", error_messages={'required': "Veuillez remplir ce champ*"})
+    password2 = forms.CharField(label="Confirmez le mot de passe",
+                                error_messages={'required': "Veuillez remplir ce champ*"})
